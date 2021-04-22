bb76d8f4f9a9d1b7a994f480a015929cc610ee65
Kaku <akash.sharma@rtcamp.com>
2021-04-22 12:25:43 +0530

    Query update

--- api/getProductsWithFilters.js
+++ api/getProductsWithFilters.js
@@ -4,7 +4,7 @@ export default function getProductsWithFilters (pageNo,categories) {
 	const categoryList = categories.join(',');
 	let totalPages=0;
 	return fetch(`https://www.houstonzoo.org/wp-json/wp/v2/posts?_embed&per_page=4&page=${pageNo}
-					${categories ? `&categories=${categoryList}` : ''}`).
+					${categories.length > 0 ? `&categories=${categoryList}` : ''}`).
 			then(res=> {
 				totalPages=res.headers.get('X-WP-TotalPages');
 				return res.json() }).
